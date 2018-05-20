18171e641c7aaae270ba34398d57c49461250006
Adam Gajzlerowicz <nelf86@gmail.com>
2018-05-20 17:55:12 +0200

    more content

--- src/components/home.js
+++ src/components/home.js
@@ -4,11 +4,41 @@ import Steam from 'react-icons/lib/fa/steam';
 import Twitter from 'react-icons/lib/fa/twitter';
 import LinkedIn from 'react-icons/lib/fa/linkedin';
 import Github from 'react-icons/lib/fa/github';
+import Email from 'react-icons/lib/fa/envelope';
 import { Col } from 'react-bootstrap';
 import Flag from 'react-world-flags'
 
 
 import Article from './article';
+
+
+/**
+  corporate and colaboration
+  SME Professional
+  Performance Horizon
+  Shirts with buttons
+  CSV extractor
+
+  fun:
+  Czy jest tarta
+  O ktorej jest obiad
+  Gajzlerek
+  Skeletoon
+  Another day another word
+
+  plugins:
+  React Select
+  React Infini Grid
+  Youtube muter chrome plugin
+
+  solo:
+  Chata małolata
+  Wilson cater hire
+  Lyles-Sutherland CRM
+  New Future Me document database
+
+*/
+
 
 // categories: fun, corporate, plugins, solo
 const articles = [
@@ -75,6 +105,14 @@ const t = {
     pl: 'Czym się zajmuję',
     en: 'What I do',
   },
+  motivationHeading: {
+    pl: 'Moja motywacja',
+    en: 'Motivation behind my work'
+  },
+  motivation: {
+    pl: 'Lubię budować aplikacje które ułatwiają pracę i sprawiają że wykonywane zadania stają się  przyjemniejsze.',
+    en: 'I like to create things that make people\'s live easier'
+  }
 }
 
 class Home extends Component {
@@ -107,36 +145,53 @@ class Home extends Component {
         <main className="container">
 
           <section className="section-dark">
-            <h1 className="section-heading">{t.pastProjects[this.state.language]}</h1>
-              {articles.map((article)=><Article key={article.title.pl} {...article} language={this.state.language}/>)}
+            <h1 className="section-heading-light">{t.motivationHeading[this.state.language]}</h1>
+            <div className="col-sm-6 col-sm-push-3 col-xs-12">
+              <article className="center-text">
+                <p className="article-content-light"> {t.motivation[this.state.language]} </p>
+              </article>
+            </div>
           </section>
 
           <section className="section-light">
-            <h1 className="section-heading-dark">{t.whatIDo[this.state.language]}</h1>
-            <article>
-              <dl>
-                <dt>Front-end</dt>
-                  <dd>Javascript, React, Redux, CSS3, ES6</dd>
-                <dt>Back-end</dt>
-                  <dd>Rest API, Graphql, NodeJS, PHP, Relational Databases</dd>
-                <dt>Apps</dt>
-                  <dd>Mobile, Electron</dd>
-                <dt>CMS</dt>
-                  <dd>Home made CMS</dd>
-                </dl>
-              </article>
+            <div className="col-sm-6 col-sm-push-3 col-xs-12">
+              <h1 className="section-heading-dark">{t.whatIDo[this.state.language]}</h1>
+              <p>I create awesome web applications!</p>
+              <p>For those, I use using following technologies</p>
+              <article className="center-text">
+                <dl>
+                  <dt>Front-end</dt>
+                    <dd>Javascript, React, Redux, CSS3, ES6</dd>
+                  <dt>Back-end</dt>
+                    <dd>Rest API, Graphql, NodeJS, PHP, Relational Databases</dd>
+                  <dt>Apps</dt>
+                    <dd>Mobile, Electron</dd>
+                  <dt>CMS</dt>
+                    <dd>Home made CMS</dd>
+                  <dt>Toolkit</dt>
+                    <dd>Vim, PHP storm, Sublime text</dd>
+                  </dl>
+                </article>
+            </div>
           </section>
+
+          <section className="section-dark">
+            <h1 className="section-heading-light">{t.pastProjects[this.state.language]}</h1>
+              {articles.map((article)=><Article key={article.title.pl} {...article} language={this.state.language}/>)}
+          </section>
+
 
         </main>
         <footer>
           <Col sm={4}> © {new Date().getFullYear()} Adam Gajzlerowicz </Col>
 
           <Col sm={8} className="footer-social">
-            <Twitter />
-            <Steam />
-            <LinkedIn />
-            <Github />
-            <StackOverflow  />
+            <a href="https://twitter.com/nelf86"><Twitter /></a>
+            <a href="https://steamcommunity.com/id/entity02"><Steam /></a>
+            <a href="https://www.linkedin.com/in/adam-gajzlerowicz-26311934"><LinkedIn /></a>
+            <a href="https://github.com/adamgajzlerowicz"><Github /></a>
+            <a href="https://stackoverflow.com/users/1333744/adam"><StackOverflow /></a>
+            <a href="mailto:nelf86@gmail.com"><Email /></a>
           </Col>
 
         </footer>
