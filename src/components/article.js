6bd8bdd96f3a0604dc8b442e3fcee80fc7075d71
Adam Gajzlerowicz <nelf86@gmail.com>
2018-05-21 18:51:10 +0200

    push

--- src/components/article.js
+++ src/components/article.js
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import { Col } from 'react-bootstrap';
 
-const Article = ({ image, description, title, category, language})=>{
-    const imagePath = require('../images/' + image);
-    return (
-        <div className="full-height">
-            <article className="col-sm-6 col-sm-push-3 col-xs-12">
-                <Col xs={12} md={4}> <img src={imagePath} className="promo-image" alt="logo" /> </Col>
-                <Col xs={12} md={8}>
-                    <h2 className="article-heading">{title[language]}</h2>
-                    <p className="article-content">{description[language]}</p>
-                </Col>
-                <Col xs={12}> <hr/> </Col>
-            </article>
-        </div>
-    );
+const Article = ({
+  image, description, title, category, language,
+}) => {
+  const imagePath = require(`../images/${image}`);
+  return (
+    <div className="full-height">
+      <article className="col-sm-6 col-sm-push-3 col-xs-12">
+        <Col xs={12} md={4}> <img src={imagePath} className="promo-image" alt="logo" /> </Col>
+        <Col xs={12} md={8}>
+          <h2 className="article-heading">{title[language]}</h2>
+          <p className="article-content">{description[language]}</p>
+          <p className="article-category">{category}</p>
+        </Col>
+        <Col xs={12}> <hr /> </Col>
+      </article>
+    </div>
+  );
 };
 
 export default Article;
--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,5 @@
+/* global sessionStorage */
+
 import React, { Component } from 'react';
 import StackOverflow from 'react-icons/lib/fa/stack-overflow';
 import Steam from 'react-icons/lib/fa/steam';
@@ -7,7 +9,7 @@ import Github from 'react-icons/lib/fa/github';
 import Email from 'react-icons/lib/fa/envelope';
 import Down from 'react-icons/lib/fa/angle-down';
 import { Col } from 'react-bootstrap';
-import Flag from 'react-world-flags'
+import Flag from 'react-world-flags';
 
 
 import Article from './article';
@@ -40,7 +42,7 @@ import Article from './article';
 
 */
 
-const getClassName = index => index % 2 === 0 ? 'dark' : 'light';
+const getClassName = index => (index % 2 === 0 ? 'dark' : 'light');
 
 
 // categories: fun, corporate, plugins, solo
@@ -48,86 +50,86 @@ const articles = [
   {
     image: 'placeholder.png',
     description: {
-      pl : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
-      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.'
+      pl: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
+      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.',
     },
     title: {
       pl: 'le name1',
-      en: 'le name1'
+      en: 'le name1',
     },
-    category: 'application'
+    category: 'application',
   },
   {
     image: 'placeholder.png',
     description: {
-      pl : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
-      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.'
+      pl: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
+      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.',
     },
     title: {
       pl: 'le name2',
-      en: 'le name2'
+      en: 'le name2',
     },
-    category: 'application'
+    category: 'application',
   },
   {
     image: 'placeholder.png',
     description: {
-      pl : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
-      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.'
+      pl: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
+      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.',
     },
     title: {
       pl: 'le name3',
-      en: 'le name3'
+      en: 'le name3',
     },
-    category: 'application'
+    category: 'application',
   },
   {
     image: 'placeholder.png',
     description: {
-      pl : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
-      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.'
+      pl: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam ad deleniti temporibus libero pariatur, ratione, facere accusantium cumque vitae, nostrum at atque modi cum amet non, sit consequuntur enim! Maxime.',
+      en: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas blanditiis itaque quaerat, dolore voluptatum, debitis laborum placeat voluptates! Eos reiciendis nulla esse expedita totam vero at quae ullam! Explicabo, soluta.',
     },
     title: {
       pl: 'le name4',
-      en: 'le name4'
+      en: 'le name4',
     },
-    category: 'application'
+    category: 'application',
   },
 ];
 
 const t = {
   jobTitle: {
     pl: 'Inżynier oprogramowania',
-    en: 'Software developer'
+    en: 'Software developer',
   },
   pastProjects: {
     pl: 'Projekty',
-    en: 'Past projects'
+    en: 'Past projects',
   },
-  whatIDo:{
+  whatIDo: {
     pl: 'Co buduje?',
     en: 'What I create?',
   },
   motivationHeading: {
     pl: 'Moja motywacja',
-    en: 'Motivation behind my work'
+    en: 'Motivation behind my work',
   },
   motivation: {
     pl: 'Lubię budować aplikacje które ułatwiają pracę i sprawiają że wykonywane zadania stają się  przyjemniejsze.',
-    en: 'I like to create things that make people\'s live easier'
+    en: 'I like to create things that make people\'s live easier',
   },
   toolsIUse: {
     pl: 'Wybrane technologie',
-    en: 'Tools I use'
+    en: 'Tools I use',
   },
   whatICreate: {
     en: 'I create awesome web applications!',
-    pl: 'Fantastyczne aplikacje internetowe!'
-  }
-}
+    pl: 'Fantastyczne aplikacje internetowe!',
+  },
+};
 
 class Home extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     const language = sessionStorage.getItem('language') || 'pl';
@@ -142,12 +144,13 @@ class Home extends Component {
       <div className="app">
 
         <div
-          onClick={()=>{
-            this.setState({ language: otherLangage })
+          onClick={() => {
+            this.setState({ language: otherLangage });
             sessionStorage.setItem('language', otherLangage);
           }}
-          className="language">
-            <Flag code={ otherLangage === 'pl' ? 'pl' : 'gb' } height="16"/>
+          className="language"
+        >
+          <Flag code={otherLangage === 'pl' ? 'pl' : 'gb'} height="16" />
         </div>
 
 
@@ -160,26 +163,25 @@ class Home extends Component {
 
         <main className="container">
 
-          <section id="what-i-do" className={`section-${getClassName(index++)}`}>
+          <section id="what-i-do" className={`center-text section-${getClassName(index++)}`}>
             <div className="col-sm-6 col-sm-push-3 col-xs-12">
               <h1 className="section-heading">{t.whatIDo[this.state.language]}</h1>
               <p className="center-text article-content">{t.whatICreate[this.state.language]}</p>
               <p className="center-text article-content"> {t.motivation[this.state.language]} </p>
-              <a href="#what-i-do" className="down"><Down /></a>
+              <a href="#portfolio" className="down"><Down /></a>
             </div>
           </section>
 
-
-          <section className={`section-${getClassName(index++)}`}>
+          <section id="portfolio" className={`section-${getClassName(index++)}`}>
             <h1 className="section-heading">{t.pastProjects[this.state.language]}</h1>
-              {articles.map((article)=><Article key={article.title.pl} {...article} language={this.state.language}/>)}
+            {articles.map(article => <Article key={article.title.pl} {...article} language={this.state.language} />)}
           </section>
 
         </main>
         <footer>
-          <Col sm={4}> © {new Date().getFullYear()} Adam Gajzlerowicz </Col>
+          <Col sm={4} xs={12}> © {new Date().getFullYear()} Adam Gajzlerowicz </Col>
 
-          <Col sm={8} className="footer-social">
+          <Col sm={8} xs={12} className="footer-social">
             <a href="https://twitter.com/nelf86"><Twitter /></a>
             <a href="https://steamcommunity.com/id/entity02"><Steam /></a>
             <a href="https://www.linkedin.com/in/adam-gajzlerowicz-26311934"><LinkedIn /></a>
