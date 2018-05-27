da72276f9aedca2fd581476a7932a4305340ba40
Adam Gajzlerowicz <nelf86@gmail.com>
2018-05-27 07:24:42 +0200

    push

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,16 @@
-/* eslint-disable-next-line */
-const scrollToView = id => window.scroll({ top: document.getElementById(id).offsetTop, behavior: 'smooth' });
+
+function getPosition(element) {
+    var yPosition = 0;
+
+    while(element) {
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return yPosition
+}
+
+/* eslint-disable-next-line*/
+const scrollToView = id => window.scroll({ top: getPosition(document.getElementById(id)), behavior: 'smooth' });
 
 export { scrollToView };
